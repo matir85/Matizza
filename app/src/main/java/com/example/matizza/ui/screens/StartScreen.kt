9dc21b38d93e96e6b354e07f09e8bc49cbde72b4
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-05-15 00:57:24 +0200

    Styling the first text box

--- app/src/main/java/com/example/matizza/ui/screens/StartScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/StartScreen.kt
@@ -10,12 +10,20 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 import com.example.matizza.R
 
 @Composable
@@ -40,7 +48,14 @@ fun StartScreen(modifier: Modifier = Modifier) {
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom) {
-        Text(text = "Co kolwiek")
+        Text(
+            modifier = Modifier.align(CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            text = "Matizza"
+        )
         Text(text = "Co kolwiek")
         OutlinedButton(onClick = { /*TODO*/ }) {
             
