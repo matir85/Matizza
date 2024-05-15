d604da114ce9d4259e343e3a03ebecef6e6c192a
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-05-15 22:59:03 +0200

    login screen initialization

---
+++ app/src/main/java/com/example/matizza/ui/screens/LoginScreen.kt
@@ -0,0 +1,16 @@
+package com.example.matizza.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+LoginScreen()
+}
