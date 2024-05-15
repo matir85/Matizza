934abc31cb93201637f2148fead7bcc603f5f52f
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-05-15 19:06:56 +0200

    creating and styling a button

--- app/src/main/java/com/example/matizza/ui/screens/StartScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/StartScreen.kt
@@ -5,8 +5,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,8 +22,12 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 
@@ -26,9 +36,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 import com.example.matizza.R
+import com.example.matizza.ui.theme.Green800
 
 @Composable
-fun StartScreen(modifier: Modifier = Modifier) {
+fun StartScreen(onStartClick: () -> Unit = {}) {
     // zmienna na zdjęcie
     val image = ImageBitmap.imageResource(id = R.drawable.start_screen_background)
 
@@ -60,14 +71,37 @@ fun StartScreen(modifier: Modifier = Modifier) {
             text = "Matizza"
         )
         Text(
-            modifier = Modifier.padding(),
+            modifier = Modifier.padding(bottom = 100.dp),
             textAlign = TextAlign.Center,
             fontSize = 25.sp,
             color = Color.White,
             text = "Pizza domowa"
         )
-        OutlinedButton(onClick = { /*TODO*/ }) {
-
+        OutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, bottom = 100.dp)
+                .height(48.dp),
+            shape = RoundedCornerShape(20),
+            colors = ButtonDefaults.buttonColors(Green800),
+            onClick = { onStartClick() }
+        ) {
+            // zmienna zawierająca wektor strzałki w prawo
+            val iconArrow = ImageVector.vectorResource(id = R.drawable.ic_arrow_right)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                color = Color.White,
+                text = "Zaloguj się"
+            )
+            Icon(
+                modifier = Modifier.padding(start = 10.dp),
+                imageVector = iconArrow,
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
         }
     }
 }
