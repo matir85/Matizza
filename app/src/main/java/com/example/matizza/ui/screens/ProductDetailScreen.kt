4010e4042ae691165afc3120cf685f8aa2026285
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-05-27 22:31:48 +0200

    creating a Product Header, previewing the Product Header

--- app/src/main/java/com/example/matizza/ui/screens/ProductDetailScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/ProductDetailScreen.kt
@@ -1,12 +1,28 @@
 package com.example.matizza.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.matizza.R
+import com.example.matizza.ui.theme.Neutral100
+
 
 @Composable
 fun ProductDetailScreen() {
@@ -17,7 +33,7 @@ fun ProductDetailScreen() {
             .fillMaxSize()
             .verticalScroll(scrollState)
     ) {
-ProductHeader()
+        ProductHeader()
         ProductImage()
         ProductDetail()
     }
@@ -25,15 +41,42 @@ ProductHeader()
 
 @Composable
 fun ProductDetail() {
-    TODO("Not yet implemented")
+
 }
 
 @Composable
 fun ProductImage() {
-    TODO("Not yet implemented")
+
 }
 
 @Composable
 fun ProductHeader() {
-    TODO("Not yet implemented")
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp, start = 16.dp, end = 16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_arrow_left),
+            contentDescription = null
+        )
+        Icon(
+            modifier = Modifier
+                .size(35.dp, 35.dp)
+                .clip(CircleShape)
+                .background(Neutral100),
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_favourite_border),
+            contentDescription = null
+        )
+    }
 }
+
+
+@Preview(showBackground = true)
+@Composable
+fun ProductHeaderView() {
+    ProductHeader()
+}
+
