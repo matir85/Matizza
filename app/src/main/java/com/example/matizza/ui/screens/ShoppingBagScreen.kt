21bff9fa175cde0766a2fcd66643343ab889621a
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-05-30 13:04:26 +0200

    Creating a template and a preview

--- app/src/main/java/com/example/matizza/ui/screens/ShoppingBagScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/ShoppingBagScreen.kt
@@ -1,9 +1,36 @@
 package com.example.matizza.ui.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ShoppingBagScreen(modifier: Modifier = Modifier) {
+    Column {
+        Box(modifier = Modifier.padding(start = 16.dp, top = 25.dp)){
+            Text(
+                text = "Koszyk",
+                fontWeight = FontWeight.Bold,
+                fontSize = 25.sp
+            )
+        }
+        ShoppingBagList()
+        SumUP()
+    }
+}
+
+@Composable
+fun SumUP() {
+
+}
+
+@Composable
+fun ShoppingBagList() {
 
 }
