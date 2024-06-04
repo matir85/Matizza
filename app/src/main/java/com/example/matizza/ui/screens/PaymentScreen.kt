a7983df32dd25322a4aaebd612d3f7d5d5b055b0
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-06-04 13:00:05 +0200

    Completion of Payment Card Detail

--- app/src/main/java/com/example/matizza/ui/screens/PaymentScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/PaymentScreen.kt
@@ -2,12 +2,17 @@ package com.example.matizza.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -49,7 +54,34 @@ fun PaymentAddress() {
 
 @Composable
 fun PaymentCardDetail() {
-
+    Surface(
+        modifier = Modifier
+            .wrapContentWidth()
+            .padding(horizontal = 16.dp),
+        shadowElevation = 1.dp,
+        shape = RoundedCornerShape(10)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .padding(vertical = 20.dp, horizontal = 16.dp)
+                    .size(35.dp, 35.dp),
+                bitmap = ImageBitmap.imageResource(id = R.drawable.ic_visa_logo),
+                contentDescription = null)
+            Column(
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(text = " **** **** 2569")
+                Text(text = "Metoda płatności")
+            }
+        }
+    }
 }
 
 @Composable
@@ -84,11 +116,12 @@ fun PaymentHeadder(onClose: () -> Unit = {}) {
 
 @Preview(showBackground = true)
 @Composable
-fun PaymentCadrDetailPreview(){
+fun PaymentCadrDetailPreview() {
     PaymentCardDetail()
 }
+
 @Preview(showBackground = true)
 @Composable
-fun PaymentHeaderPreview(){
+fun PaymentHeaderPreview() {
     PaymentHeadder()
 }
