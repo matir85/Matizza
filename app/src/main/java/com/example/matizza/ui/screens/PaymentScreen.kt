940ba130f52bf8bcdf3f1f3fd9c751bfdea242a7
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-06-04 13:19:43 +0200

    Completion of Payment Addres and preview

--- app/src/main/java/com/example/matizza/ui/screens/PaymentScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/PaymentScreen.kt
@@ -1,6 +1,8 @@
 package com.example.matizza.ui.screens
 
+import android.location.Address
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,13 +14,19 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,8 +56,44 @@ fun PaymentTotalCost() {
 }
 
 @Composable
-fun PaymentAddress() {
+fun PaymentAddress(address: String = "") {
+    val placeIconn = ImageVector.vectorResource(id = R.drawable.ic_place)
+    val editIcon = ImageBitmap.imageResource(id = R.drawable.ic_edit)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    modifier = Modifier.padding(end = 16.dp),
+                    imageVector = placeIconn,
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(Color.Red)
+                )
+                Column(horizontalAlignment = Alignment.Start) {
+                    Text(text = "Adres", fontWeight = FontWeight.Bold)
+                    Text(text = address, fontWeight = FontWeight.Light)
+                }
+            }
+        }
 
+        IconButton(
+            modifier = Modifier.border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = RoundedCornerShape(10)
+            ), onClick = { }) {
+            Icon(
+                modifier = Modifier.size(25.dp, 25.dp),
+                bitmap = editIcon,
+                contentDescription = null
+            )
+        }
+    }
 }
 
 @Composable
@@ -73,7 +117,8 @@ fun PaymentCardDetail() {
                     .padding(vertical = 20.dp, horizontal = 16.dp)
                     .size(35.dp, 35.dp),
                 bitmap = ImageBitmap.imageResource(id = R.drawable.ic_visa_logo),
-                contentDescription = null)
+                contentDescription = null
+            )
             Column(
                 horizontalAlignment = Alignment.Start
             ) {
@@ -112,6 +157,12 @@ fun PaymentHeadder(onClose: () -> Unit = {}) {
             )
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PaymentAddressPreview() {
+    PaymentAddress(address = "Poniec, ul. Rynek 3")
 }
 
 @Preview(showBackground = true)
