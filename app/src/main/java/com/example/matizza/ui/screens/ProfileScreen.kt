3d51b4a028debbbd86073fa9e8d4f73fc92e9f90
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-06-04 18:00:59 +0200

    creating a menu profile

--- app/src/main/java/com/example/matizza/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/ProfileScreen.kt
@@ -1,22 +1,35 @@
 package com.example.matizza.ui.screens
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.matizza.R
 import com.example.matizza.data.UiState
 import com.example.matizza.data.samples.sampleProfile
+import com.example.matizza.ui.theme.Green800
 
 @Composable
 fun ProfileScreen(
@@ -35,6 +48,37 @@ fun ProfileScreen(
 }
 
 @Composable
+fun ProfileHelp() {
+
+}
+
+@Composable
+fun ProfileMenu() {
+    Column {
+        ProfileButton(
+            resourceId = R.drawable.ic_history,
+            buttonText = "Historia",
+            onClick = {}
+        )
+        ProfileButton(
+            resourceId = R.drawable.ic_profile,
+            buttonText = "Profil",
+            onClick = {}
+        )
+        ProfileButton(
+            resourceId = R.drawable.ic_address,
+            buttonText = "Adres",
+            onClick = {}
+        )
+        ProfileButton(
+            resourceId = R.drawable.ic_payments,
+            buttonText = "Płatność",
+            onClick = {}
+        )
+    }
+}
+
+@Composable
 fun ProfileHeader() {
     val arrowLeft = ImageVector.vectorResource(id = R.drawable.ic_arrow_left)
 
@@ -48,17 +92,51 @@ fun ProfileHeader() {
 }
 
 @Composable
-fun ProfileMenu() {
+fun ProfileButton(
+    @DrawableRes resourceId: Int,
+    buttonText: String,
+    onClick: () -> Unit
+) {
+    val arroeRight = ImageVector.vectorResource(id = R.drawable.ic_arrow_right)
+    val displayIcon = ImageBitmap.imageResource(id = resourceId)
 
+    OutlinedButton(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        colors = ButtonDefaults.buttonColors(Color.White),
+        onClick = onClick,
+        shape = RoundedCornerShape(10)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(25.dp, 25.dp)
+                    .padding(end = 10.dp),
+                bitmap = displayIcon,
+                contentDescription = null
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                text = buttonText,
+                textAlign = TextAlign.Start,
+                color = Color.Black)
+            Image(
+                imageVector = arroeRight,
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Green800)
+            )
+        }
+    }
 }
 
-@Composable
-fun ProfileHelp() {
 
-}
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun ProfileScreenPreview(){
+fun ProfileScreenPreview() {
     ProfileScreen(data = sampleProfile)
 }
