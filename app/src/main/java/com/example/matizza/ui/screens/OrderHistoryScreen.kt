977418b55f8ea8604a684d0862e8974585bc05cd
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-06-06 23:22:00 +0200

    Creation of Order History with preview, Completing the entire Order History Screen

--- app/src/main/java/com/example/matizza/ui/screens/OrderHistoryScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/OrderHistoryScreen.kt
@@ -2,22 +2,32 @@ package com.example.matizza.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +37,7 @@ import com.example.matizza.R
 import com.example.matizza.data.Order
 import com.example.matizza.data.UiState
 import com.example.matizza.data.samples.sampleEmptyOrderHistoryData
+import com.example.matizza.data.samples.sampleOrderHistoryData
 import com.example.matizza.ui.theme.Green800
 
 @Composable
@@ -43,7 +54,70 @@ fun OrderHistoryScreen(
 
 @Composable
 fun OrderHistory(orderList: List<Order>) {
+    Column {
+        OrdersHistoryHeader(orderNumber = orderList.size)
+        LazyColumn(contentPadding = PaddingValues(bottom = 100.dp)) {
+            items(items = orderList,
+                key = { order: Order -> order.item.id }) { order ->
+                OrderHistoryItem(order)
+            }
+        }
+    }
+}
 
+@Composable
+fun OrderHistoryItem(order: Order) {
+    Surface(
+        modifier = Modifier
+            .wrapContentWidth()
+            .padding(10.dp),
+        shadowElevation = 1.dp,
+        shape = RoundedCornerShape(10)
+    ) {
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(
+                modifier = Modifier.padding(bottom = 2.dp),
+                text = "${order.item.orderState}, ${order.item.date}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            Text(
+                text = "${order.item.name}...",
+                fontWeight = FontWeight.Light
+            )
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomEnd) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        modifier = Modifier.padding(20.dp),
+                        text = (order.item.price * order.count).toString(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 25.sp,
+                        color = Green800
+                    )
+                    val arrowRights = ImageVector.vectorResource(id = R.drawable.ic_arrow_right)
+                    Icon(imageVector = arrowRights, contentDescription = null, tint = Green800)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun OrdersHistoryHeader(orderNumber: Int) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 16.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(
+            text = "Historia zamówień ($orderNumber)",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+        )
+    }
 }
 
 @Composable
@@ -91,4 +165,10 @@ fun EmptyOrderHistory(onEmptyHistoryClick: () -> Unit) {
 @Composable
 fun EmptyOrderHistoryPreview() {
     OrderHistoryScreen(data = sampleEmptyOrderHistoryData)
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun OrderHistoryPreview() {
+    OrderHistoryScreen(data = sampleOrderHistoryData)
 }
