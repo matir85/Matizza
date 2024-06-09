bc822dbb03d6a34d4570396a641c755b14c2113d
Mateusz Rusakowicz <mateusz.rusakowicz@gmail.com>
2024-06-09 18:46:25 +0200

    Adding animation to InfoCard

--- app/src/main/java/com/example/matizza/ui/screens/MapScreen.kt
+++ app/src/main/java/com/example/matizza/ui/screens/MapScreen.kt
@@ -1,10 +1,10 @@
 package com.example.matizza.ui.screens
 
-import android.location.Address
-import android.widget.ImageButton
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -32,11 +33,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.matizza.R
@@ -71,9 +74,18 @@ fun MapScreen(
             )
             OrderMap()
         }
-        AnimatedVisibility(visible = isDetailVisable, enter = scaleIn(), exit = scaleOut()) {
+        AnimatedVisibility(
+            visible = isDetailVisable,
+            enter = slideIn { IntOffset(0, 500) } + fadeIn(),
+            exit = slideOut{IntOffset(0,500)} + fadeOut()
+        ) {
             Box(modifier = Modifier, contentAlignment = Alignment.BottomCenter) {
-                InfoCard()
+                InfoCard(
+                    name = data.name,
+                    surname = data.surname,
+                    sourceAddress = data.sourceAddress,
+                    targetAddress = data.targetAddress
+                )
             }
         }
         Box(contentAlignment = Alignment.BottomCenter) {
@@ -147,7 +159,8 @@ fun InfoCard(
             Surface(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 10.dp),
+                    .padding(horizontal = 16.dp, vertical = 30.dp)
+                    .offset(y = -20.dp),
                 shape = RoundedCornerShape(12),
                 shadowElevation = 1.dp
             ) {
@@ -158,7 +171,24 @@ fun InfoCard(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.SpaceBetween
                 ) {
-                    InfoCardRow()
+                    InfoCardRow(
+                        modifier = Modifier.padding(start = 16.dp, top = 16.dp),
+                        bitmap = placeIcon,
+                        address = sourceAddress
+                    )
+                    Image(
+                        modifier = Modifier
+                            .padding(start = 25.dp)
+                            .size(30.dp, 70.dp),
+                        bitmap = ImageBitmap.imageResource(id = R.drawable.ic_line),
+                        contentDescription = null,
+                        colorFilter = ColorFilter.tint(Color.LightGray)
+                    )
+                    InfoCardRow(
+                        modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
+                        bitmap = clockImage,
+                        address = targetAddress
+                    )
                 }
             }
         }
@@ -167,7 +197,9 @@ fun InfoCard(
 
 @Composable
 fun InfoCardRow(
-    modifier: Modifier = Modifier, bitmap: ImageBitmap, address: String
+    modifier: Modifier = Modifier,
+    bitmap: ImageBitmap,
+    address: String
 ) {
     Row(modifier = modifier) {
         IconButton(
